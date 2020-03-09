c55348c23506c232778b3b3f7dc40e482aecc181
Trond <Trond@10.22.15.23>
2020-03-09 16:16:12 +0100

    tester

---
+++ Datamodelleringsprosjekt/src/datamodelleringsprosjekt/kontrollerTest.java
@@ -0,0 +1,19 @@
+package datamodelleringsprosjekt;
+
+import java.sql.*;
+import java.util.*;
+
+public class kontrollerTest extends Driver{
+	
+	public void finnRoller(String skuespiller) {
+		try {
+			
+		}
+		catch (Exception e) {
+			System.out.println("Finner ikke skuespiller");
+		}
+	}
+	
+	
+
+}
