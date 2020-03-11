06eed3dcf0269b5e2ea792e81b5fec06a99c7909
carlaxellind <carlaxellind@dhcp-10-24-4-103.wlan.ntnu.no>
2020-03-11 11:56:05 +0100

    Startside

--- Datamodelleringsprosjekt/src/datamodelleringsprosjekt/Driver.java
+++ Datamodelleringsprosjekt/src/datamodelleringsprosjekt/Driver.java
@@ -1,7 +1,9 @@
 package datamodelleringsprosjekt;
 
 import java.sql.*;
+import java.util.Scanner;
 public class Driver {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,8 +18,79 @@ public class Driver {
 			ResultSet myRs = myStmt.executeQuery("select * from Media");
 					
 			//4. Process the result set
-			while (myRs.next()) {
+			/*while (myRs.next()) {
 				System.out.println(myRs.getString("Tittel") + ", " + myRs.getString("Utgivelsesår"));
+			}
+			*/
+			//5: Hva ønsker du å gjøre?
+			Scanner myScann = new Scanner(System.in);
+			System.out.println("What do you wish to do, answer with number 1 to 5: "
+					+ "\n 1 - Find the name of all the rolles a actor has played. "
+					+ "\n 2 - Find what movies a given actor has played in. "
+					+ "\n 3 - Find the company who has made the most movies in a giveen genre"
+					+ "\n 4 - Add a new movie with director, screenwriter, actor and everything else. "
+					+ "\n 5 - Add a new review of an episode in a serie.");
+			
+			int todo = myScann.nextInt();
+			
+			if (todo == 1) {
+				
+			}
+			
+			else if (todo == 2) {
+				//6: Filmer en gitt skuespiller opptrer i
+				Scanner myScanner = new Scanner(System.in);
+				System.out.println("Enter actor");
+				ResultSet personID;
+				
+				String actor = myScanner.nextLine();
+				
+				ResultSet myRs1 = myStmt.executeQuery("select * from Person");
+				String name = myRs1.getString("FulltNavn");
+				
+				ResultSet myRs2 = myStmt.executeQuery("select * from Skuespiller");
+				String ID = myRs2.getString("FulltNavn");
+				if(name.equals(actor))
+				{
+					
+				}
+				myScanner.close();
+			}
+			
+			else if (todo == 3) {
+				Scanner myScn = new Scanner(System.in);
+				
+				System.out.println("Enter Title, length of movie, Release year, Release date, Description, Form, Published:");
+				String t = myScn.nextLine();
+				String l = myScn.nextLine();
+				String RY = myScn.nextLine();
+				String RD = myScn.nextLine();
+				String d = myScn.nextLine();
+				String f = myScn.nextLine();
+				String p = myScn.nextLine();
+				
+				ResultSet myRs3 = myStmt.executeQuery("select * from Media");
+				int counter = 1;
+				
+				while(myRs3.next()) {
+					counter+=1;
+					
+				}
+				
+				String sq1 = "insert into Media" + "(MediaID, Tittel, Lengde, Utgivelsesår, Lanseringsdato, Beskrivelse, Form, Utgitt)" + " values ('"+counter+"','"+t+"','"+l+"','"+RY+"','"+RD+"','"+d+"','"+f+"','"+p+"')";
+				
+				myStmt.executeUpdate(sq1);
+				
+				System.out.println("Insert complete");
+				
+			}
+			
+			else if (todo == 4) {
+				
+			}
+			
+			else {
+				System.out.println("Invalid entry");
 			}
 		}
 		catch (Exception exc) {
--- Datamodelleringsprosjekt/src/datamodelleringsprosjekt/newMovie.java
+++ Datamodelleringsprosjekt/src/datamodelleringsprosjekt/newMovie.java
@@ -53,3 +53,58 @@ public class newMovie {
 	}
 
 }
+
+
+
+/* 
+package datamodelleringsprosjekt;
+import java.sql.*;
+import java.util.*;
+
+public class Person extends DBConn {
+	private int PersonID;
+	private String FulltNavn;
+	private int Fodt�r;
+	private String F�dtLand;
+	
+	public Person (int PersonID) {
+		this.PersonID = PersonID;
+	}
+	
+	public int getPersonID() {
+		return PersonID;
+	}
+	
+	public void initialize () {
+		connect();
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select FulltNavn, F�dsels�r, F�dselsland from Person where PersonID=" + PersonID);
+            while (rs.next()) {
+                FulltNavn =  rs.getString("FulltNavn");
+                F�dt�r = rs.getInt("F�dsels�r");
+                F�dtLand = rs.getString("F�dselsland");
+            }
+
+        } catch (Exception e) {
+            System.out.println("db error during select of bruker= "+e);
+            return;
+            }
+	}
+	
+	
+	public void refresh () {
+        initialize ();
+    }
+	
+	
+	public static void main(String[] args) {
+		Person test = new Person(2);
+		System.out.println(test.getPersonID());
+		test.initialize();
+		System.out.println(test.FulltNavn);
+		System.out.println(test.F�dtLand);
+	}
+
+}
+*/
